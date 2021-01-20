31ce0999ed717914f0ccd93b5a30fb85dda20546
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-20 19:43:16 -0300

    Waypoint  - Spawner Random and Fixed

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnModes
+{
+    Fixed,
+    Random
+}
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
+    [SerializeField] private int enemyCount = 10;
+
+    [Header("Fixed Delay")]
+    [SerializeField] private GameObject testGameObject;
+    [SerializeField] private float delayBetweenSpawns;
+
+    [Header("RAndon Delay")]
+    [SerializeField] private float minRandomDelay;
+    [SerializeField] private float maxRandomDelay;
+
+    //
+    private float _spawnTimer;
+    private int _enemiesSpawned;
+
+    void Update()
+    {
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer < 0)
+        {
+            _spawnTimer = GetSpawnDelay();
+            if (_enemiesSpawned < enemyCount)
+            {
+                _enemiesSpawned++;
+                SpawnEnemy();
+            }
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(testGameObject, transform.position, Quaternion.identity);
+    }
+
+    private float GetSpawnDelay()
+    {
+        float delay = 0f;
+        if (spawnMode == SpawnModes.Fixed)
+        {
+            delay = delayBetweenSpawns;
+        }
+        else
+        {
+            delay = GetRandomDelay();
+        }
+
+        return delay;
+    }
+
+    private float GetRandomDelay()
+    {
+        float randomTimer = Random.Range(minRandomDelay, maxRandomDelay);
+        return randomTimer;
+    }
+}
