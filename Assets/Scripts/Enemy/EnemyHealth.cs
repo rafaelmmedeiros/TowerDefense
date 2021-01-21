435803224484b43ac4624f669c55711291532d2e
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-21 17:01:50 -0300

    Health Bar - Prefab, Some Code refactor

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private GameObject healthBarPrefab;
+    [SerializeField] private Transform barPosition;
+
+    [SerializeField] private float initialHealth = 10f;
+    [SerializeField] private float maxHealth = 10f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateHealthBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateHealthBar()
+    {
+        GameObject newBar = Instantiate(
+            healthBarPrefab,
+            barPosition.position,
+            Quaternion.identity);
+        newBar.transform.SetParent(transform);
+    }
+}
