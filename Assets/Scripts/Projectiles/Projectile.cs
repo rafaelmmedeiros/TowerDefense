bcc161bfa5ead860e7e5ae3c59b0edd096f1ac27
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-23 00:08:14 -0300

    Turret - Rotation

---
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float MoveSpeed = 10f;
+
+    private Enemy _enemyTarget;
+
+    private void Update()
+    {
+        if (_enemyTarget != null)
+        {
+            RotateProjectile();
+            MoveProjectile();
+        }
+    }
+
+    private void MoveProjectile()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            _enemyTarget.transform.position,
+            MoveSpeed * Time.deltaTime);
+    }
+
+    private void RotateProjectile()
+    {
+        Vector3 enemyPosition = _enemyTarget.transform.position - transform.position;
+        float angle = Vector3.SignedAngle(
+            transform.up,
+            enemyPosition, 
+            transform.forward);
+        transform.Rotate(0f,0f,angle);
+    }
+
+    public void SetEnemy(Enemy enemy)
+    {
+        _enemyTarget = enemy;
+    }
+}
---
+++ Assets/Scripts/Projectiles/TurretProjectile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretProjectile : MonoBehaviour
+{
+    [SerializeField] private Transform projectileSpawnPosition;
+    
+    private ObjectPooler _pooler;
+    private Turret _turret;
+    private Projectile _currentProjectileLoaded;
+
+    private void Start()
+    {
+        _turret = GetComponent<Turret>();
+        _pooler = GetComponent<ObjectPooler>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            LoadProjectile();
+        }
+
+        if (_turret.CurrentEnemyTarget != null &&
+            _currentProjectileLoaded != null &&
+            _turret.CurrentEnemyTarget.EnemyHealth.CurrentHealth > 0f)
+        {
+            _currentProjectileLoaded.transform.parent = null;
+            _currentProjectileLoaded.SetEnemy(_turret.CurrentEnemyTarget);
+        }
+    }
+
+    private void LoadProjectile()
+    {
+        GameObject newInstance = _pooler.GetInstanceFromPool();
+        newInstance.transform.localPosition = projectileSpawnPosition.position;
+        newInstance.transform.SetParent(projectileSpawnPosition);
+
+        _currentProjectileLoaded = newInstance.GetComponent<Projectile>();
+        newInstance.SetActive(true);
+    }
+}
